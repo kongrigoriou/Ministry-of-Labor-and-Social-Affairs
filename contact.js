a95d8b292c5207ac68256684f0beb49ba6e00a45
Panos <panos1029@gmail.com>
2021-01-15 15:58:51 +0200

    log in page

--- contact.js
+++ contact.js
@@ -1,4 +1,12 @@
 function myFunction() {
     var x = document.getElementById("rectangle");
     x.style.display = "block";
-  }
+}
+
+function test() {
+
+    var email = document.getElementById("exampleInputEmail1");
+    var pass = document.getElementById("exampleInputPassword1");
+
+    console.log(email.value, pass.value);
+}
